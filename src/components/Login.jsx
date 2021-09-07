8b01c2b3c79b95bee8a0cbce1d46e3186fbe0373
caarlosdamian <dapecarlos@gmail.com>
2021-09-07 16:08:01 -0500

    Feature: Add Login component

---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="container">
+      <div className="left">d</div>
+      <div className="right">d</div>
+    </div>
+  );
+};
+
+export default Login;
