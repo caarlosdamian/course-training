af4573ffc0ded8e8ff0f4c81a479b1c24991165e
caarlosdamian <dapecarlos@gmail.com>
2021-09-07 18:43:12 -0500

    Feature: At Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,65 @@
-import React from "react";
-
+import "./login.css";
+import gmail from "../assets/gmail01.png";
 const Login = () => {
   return (
     <div className="container">
-      <div className="left">d</div>
-      <div className="right">d</div>
+      <div className="left">
+        <img
+          src="https://mobirise.com/bootstrap-carousel/assets2/images/thomas-smith-399133-1707x2560.jpg"
+          alt="mountain"
+        />
+      </div>
+      <div className="right">
+        <form className="right-form">
+          <div className="form-container">
+            <div className="form-container-headers">
+              <h6 className="form-container-subheader">Welcome back</h6>
+              <h2 className="form-container-header">Login to your account</h2>
+            </div>
+            <span className="form-container-span">Email</span>
+            <input
+              className="form-container-input"
+              type="text"
+              placeholder="dapecarlos
+            @gmail.com"
+            />
+            <span className="form-container-span">Password</span>
+            <input
+              className="form-container-input"
+              type="password"
+              placeholder="********"
+            />
+            <div className="form-container-container-radio">
+              <div className="form-container-container-radio-left">
+                <input
+                  className="form-container-radio"
+                  type="radio"
+                  name=""
+                  id=""
+                />
+                <span>Remember me</span>
+
+                <div className="form-container-container-radio-left-right"></div>
+              </div>
+
+              <a href="/home" className="span-anchor">
+                {" "}
+                Forgot password?
+              </a>
+            </div>
+
+            <input
+              className="form-container-btn green"
+              type="submit"
+              value="Login"
+            />
+            <button className="form-container-btn black" type="submit">
+              <img className="form-container-icon" src={gmail} alt="" />
+              <span>Sign-in with google</span>
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
